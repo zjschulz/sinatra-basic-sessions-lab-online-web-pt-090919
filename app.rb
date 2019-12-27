2bceae60dede895c2c19a398b69bc6db2db8ec51
Zachary Schulz <zjschulz@gmail.com>
2019-12-27 01:21:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+  
+  get '/' do
+    erb :index
+  end
+  
+  post '/checkout' do 
+    session["item"] = params[:item]
+    @session = session
+  end
+  
 end
